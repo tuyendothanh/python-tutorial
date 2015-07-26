7091fc9fd2c9d12bd802f8c1258ac2893ab2aade
Tuyen Dothanh <tuyen.tkmt@gmail.com>
2015-07-26 16:54:31 +0900

    Modify priority_sort function

--- Functions/ClosuresInteract.py
+++ Functions/ClosuresInteract.py
@@ -1,9 +1,20 @@
+"""
 def priority_sort(items, group):
 	def sortHelper(x):
 		if x in group:
 			return 0, x
 		return 1, x
 	items.sort(key = sortHelper)
+"""
+def priority_sort(items, group):
+	found = [False]
+	def sortHelper(x):
+		if x in group:
+			found[0] = True
+			return 0, x
+		return 1, x
+	items.sort(key = sortHelper)
+	return found[0]
 
 class sorter(object):
 	def __init__(self, group):
@@ -19,8 +30,9 @@ class sorter(object):
 
 items = [8, 1, 2, 7, 3,5, 4, 6]
 group = {5, 7, 2}
-priority_sort(items, group)
+found = priority_sort(items, group)
 print items
+print found
 
 sort = sorter(group)
 items.sort(key = sort)
