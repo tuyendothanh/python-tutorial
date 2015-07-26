7ce67287dae6af9ccc121cb379e6d2f0382a301f
Tuyen Dothanh <tuyen.tkmt@gmail.com>
2015-07-26 16:38:22 +0900

    Add class sort to return found

--- Functions/ClosuresInteract.py
+++ Functions/ClosuresInteract.py
@@ -5,7 +5,24 @@ def priority_sort(items, group):
 		return 1, x
 	items.sort(key = sortHelper)
 
+class sorter(object):
+	def __init__(self, group):
+		self.group = group
+		self.found = False
+
+	def __call__(self, x):
+		if x in group:
+			self.found = True
+			return 0, x
+		return 1, x
+
+
 items = [8, 1, 2, 7, 3,5, 4, 6]
 group = {5, 7, 2}
 priority_sort(items, group)
 print items
+
+sort = sorter(group)
+items.sort(key = sort)
+print items
+print sort.found
